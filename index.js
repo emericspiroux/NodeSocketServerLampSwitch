1da91345c1bdbce93cf2abe07a845a8cc89c0268
emericspiroux <emeric.spiroux@gmail.com>
2017-03-27 06:11:48 +0200

    Add node server socket io

--- index.js
+++ index.js
@@ -6,22 +6,42 @@ var isOn = false
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
-
-  isOn = !isOn;
-
-  var isOnString = "1";
-  if (!isOn) {
-    isOnString = "0";
-  }
-  console.log("Status relay :" + isOnString);
-  io.emit('relay lamp', isOnString);
+  changeLampState(isOn);
 });
+
+app.get('/switchLamp', function(req, res){
+	res.send('{"isOn":'+isOn+'}');
+});
+
+app.post('/switchLamp', function(req, res){
+	changeLampState(isOn);
+   	res.send('{"isOn":'+ isOn +'}', 200);
+});
+
+function changeLampState(state){
+	if (typeof(state) === "boolean") {
+	   	isOn = !isOn;
+
+  		var isOnString = "1";
+  		if (!isOn) {
+    			isOnString = "0";
+  		}
+  		console.log("Status relay :" + isOnString);
+  		io.emit('relay lamp', isOnString);
+	}
+}
 
 io.on('connection', function(socket){
   console.log("Controller connected !")
+  
+  if (isOn == true){
+	isOn = !isOn;
+	changeLampState(isOn);
+  }	
 
-  socket.on('interruptor lamp', function(value){
-	console.log("Lamp on");
+  socket.on('change gyro', function(value){
+	console.log("Switch light !");
+	changeLampState(isOn);
   });
   
   socket.on('disconnect', function(){
@@ -30,6 +50,6 @@ io.on('connection', function(socket){
 
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(8080, function(){
+  console.log('listening on *:8080');
 });
